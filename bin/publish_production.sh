61278c063d4ef6ca9ac0228165f0fa1d01671404
Dan Berindei <dan@infinispan.org>
2014-10-20 15:49:18 +0300

    ISPN-4814 Push to the master branch explicitly to work around the problems in the awestruct github_pages deployer

--- bin/publish_production.sh
+++ bin/publish_production.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 rm -rf _site
 bin/fetch_docs.rb -f
-bundle exec awestruct -gw -Pproduction --deploy
+
+# The awestruct github_pages deployer doesn't work on the CI machine ATM, it deletes everything on the master branch
+# so we deploy manually instead
+#bundle exec awestruct -gw -Pproduction --deploy
+bundle exec awestruct -gw -Pproduction
+git checkout master
+cp -r _site/* .
+git commit -a -m "Published master to GitHub pages."
+git push origin master
