25a9f12420b4557c222407a4def79c59dabea172
Infinispan <infinispan@infinispan.org>
2021-01-25 09:00:22 -0500

    Published master to GitHub pages.

--- docs/12.0.x/topics/code_examples/QueryExample.java
+++ docs/12.0.x/topics/code_examples/QueryExample.java
@@ -1,14 +1,16 @@
-// get the query factory from the cache:
+// Get the query factory from the cache
 QueryFactory queryFactory = org.infinispan.query.Search.getQueryFactory(cache);
 
-// create an Ickle query that will do a full-text search (operator ':') on fields 'title' and 'authors.name'
-Query<Book> fullTextQuery = queryFactory.create("FROM com.acme.Book WHERE title:'infinispan' AND authors.name:'sanne'")
+// Create an Ickle query that performs a full-text search using the ':' operator on the 'title' and 'authors.name' fields
+// You can perform full-text search only on indexed caches
+Query<Book> fullTextQuery = queryFactory.create("FROM org.infinispan.sample.Book b WHERE b.title:'infinispan' AND b.authors.name:'sanne'");
 
-// The ('=') operator is not a full-text operator, thus can be used in both indexed and non-indexed caches
-Query<Book> exactMatchQuery = queryFactory.create("FROM com.acme.Book WHERE title = 'Programming Infinispan' AND authors.name = 'Sanne Grinnovero'")
+// Use the '=' operator to query fields in caches that are indexed or not
+// Non full-text operators apply only to fields that are not analyzed
+Query<Book> exactMatchQuery=queryFactory.create("FROM org.infinispan.sample.Book b WHERE b.isbn = '12345678' AND b.authors.name : 'sanne'");
 
-// Full-text and non-full text operators can be part of the same query
-Query q = queryFactory.create("FROM com.query.Book b where b.author.name = 'Stephen' and b.description : (+'dark' -'tower')");
+// You can use full-text and non-full text operators in the same query
+Query<Book> query=queryFactory.create("FROM org.infinispan.sample.Book b where b.authors.name : 'Stephen' and b.description : (+'dark' -'tower')");
 
-// get the results
+// Get the results
 List<Book> found=query.execute().list();
