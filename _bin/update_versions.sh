dac9693f911fe380063bdb875a0e24c3aa0529d3
Tristan Tarrant <ttarrant@redhat.com>
2025-01-22 11:31:55 +0100

    Use gh cli to update release info

--- _bin/update_versions.sh
+++ _bin/update_versions.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-JIRA_URL="https://issues.redhat.com/rest/api/latest"
-RELEASE_NOTES_URL="https://issues.redhat.com/secure/ReleaseNote.jspa"
-PROJECT="ISPN"
+REPO=github.com/infinispan/infinispan
 BASEDIR=$(dirname "$0")/..
 CONFIG_FILE="$BASEDIR/_data/ispn.yml"
 
@@ -10,18 +8,19 @@ update_version ()
   echo -n "Processing $1: "
   BASE_VERSION=$(yq ".$1.minor_version" "$CONFIG_FILE")
 
-  LATEST_VERSION=$(curl -s -H "Authorization: Bearer $TOKEN" "$JIRA_URL/project/$PROJECT/versions"|yq -r ".[]| select(.released==true)|select(.name | match(\"^$BASE_VERSION.\")) |.name"|sort -V|tail -n1)
-  VERSION_INFO=$(curl -s -H "Authorization: Bearer $TOKEN" "$JIRA_URL/project/$PROJECT/versions"|yq -r ".[]| select(.name==\"$LATEST_VERSION\")|[.projectId,.id,.userReleaseDate]|@csv"|tr -d \")
+  LATEST_VERSION=$(gh release list -R $REPO |cut -f1|grep "^$BASE_VERSION"|head -n1)
+  VERSION_INFO=$(gh release view -R $REPO $LATEST_VERSION --json publishedAt,url|yq -r "[.publishedAt,.url]|@csv"|tr -d \")
 
   echo "$LATEST_VERSION"
 
-  IFS=, read -r PROJECT_ID RELEASE_ID RELEASE_DATE <<< "$VERSION_INFO"
+  IFS=, read -r RELEASE_DATE URL <<< "$VERSION_INFO"
+  RELEASE_DATE=$(date -d"$RELEASE_DATE" +%Y/%m/%d)
 
   yq -i "
    .$1.version = \"$LATEST_VERSION\" |
    .$1.release_date = \"$RELEASE_DATE\" |
    .$1.maven_latest = \"$LATEST_VERSION\" |
-   .$1.fixed_issues = \"$RELEASE_NOTES_URL?projectId=$PROJECT_ID&version=$RELEASE_ID\" |
+   .$1.fixed_issues = \"$URL\" |
    .$1.download.server = \"https://downloads.jboss.org/infinispan/$LATEST_VERSION/infinispan-server-$LATEST_VERSION.zip\" |
    .$1.download.docker_native = \"quay.io/infinispan/server-native:$LATEST_VERSION-1\" |
    .$1.download.docker_cli = \"quay.io/infinispan/cli:$LATEST_VERSION-1\"
@@ -29,8 +28,7 @@ update_version ()
 }
 
 update_version "stable"
-update_version "unstable"
+#update_version "unstable"
+update_version "old.\"15.0\""
 update_version "old.\"14.0\""
-update_version "old.\"13.0\""
-update_version "old.\"12.1\""
-update_version "old.\"12.0\""
+
