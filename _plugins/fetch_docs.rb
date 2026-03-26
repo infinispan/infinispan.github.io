f25d312cbff0be3d42441dcef92ca967c7dabda9
Tristan Tarrant <tristan@infinispan.org>
2026-03-26 18:26:52 +0100

    Migrate fetch_docs.rb from ispn.yml to projects.yml
    Derive Maven doc coordinates from version data in projects.yml instead of
    duplicating them in ispn.yml. Use doc_branches lists and GitHub URLs for
    operator, helm chart, tutorials, and spring boot doc fetching.

--- _plugins/fetch_docs.rb
+++ _plugins/fetch_docs.rb
@@ -6,10 +6,10 @@ require "optimist"
 require "nokogiri"
 
 opts = Optimist::options do
-  version "fetch_docs 0.1.0 (c) The Infinispan team"
+  version "fetch_docs 0.2.0 (c) The Infinispan team"
   banner <<-EOS
 This script pulls documentation artifacts generated during Infinispan builds
-and incorporates them into the Awestruct website.
+and incorporates them into the website.
 Usage:
        bin/fetch_docs.rb [options]
 where [options] are:
@@ -25,7 +25,7 @@ verbose = opts.verbose == true
 cwd = File.expand_path File.dirname(__FILE__)
 site_home = cwd + "/../"
 puts "Using the docs directory" if verbose
-cfg = YAML.load_file("_data/ispn.yml")
+projects = YAML.load_file("_data/projects.yml")
 
 def get_docs(tmp, name, repo, branch, loc, docroot, docbase, verbose, attr_header)
   target = File.expand_path("#{docroot}#{docbase}")
@@ -118,6 +118,26 @@ def gen_versions_xml_file(filePath, sourceData)
   File.write(filePath, xmlVersionBuilder.to_xml)
 end
 
+def minor_to_s(minor)
+  minor.is_a?(Float) ? format("%.1f", minor) : minor.to_s
+end
+
+def fetch_github_docs(github_url, branch, target_dir, verbose)
+  zip_url = "#{github_url}/archive/#{branch}.zip"
+  tmp_prefix = File.basename(target_dir)
+  %x( wget -nv #{zip_url} -O _#{tmp_prefix}tmp.zip)
+  %x( unzip -o _#{tmp_prefix}tmp.zip "*documentation/*" -d _#{tmp_prefix}tmp)
+  Dir.glob("_#{tmp_prefix}tmp/**/*.asciidoc").each do |f|
+    %x( asciidoctor #{f} )
+  end
+  Dir.glob("_#{tmp_prefix}tmp/**/*.html").each do |f|
+    %x( cp -r #{f} _#{tmp_prefix}tmp )
+  end
+  FileUtils.mkdir_p target_dir
+  %x( mv _#{tmp_prefix}tmp/*.html "#{target_dir}/" )
+  %x( rm -rf _#{tmp_prefix}tmp* )
+end
+
 forceDocumentationDownload = (ENV.fetch("FORCE_DOCUMENTATION_DOWNLOAD") { "true" }).upcase
 if forceDocumentationDownload == "FALSE" and File.file?("docs/versions.xml")
   puts "Documentation exists. Skip the forced download..."
@@ -128,170 +148,136 @@ else
   operatorDocIndex = Array.new
   helmChartDocIndex = Array.new
 
-  cfg["docs"].each do |type, tcfg|
-    puts "Processing #{type}"
-    tcfg.each do |ver, vcfg|
-      puts "  #{ver}"
+  # Fetch infinispan core docs
+  infinispan = projects["projects"]["infinispan"]
+  infinispan.each do |minor, cfg|
+    next if minor == "github"
+    next unless cfg.is_a?(Hash)
+    next unless cfg["alias"] || cfg["docs"]
 
-      attr_header = ""
-      asciidocattr = vcfg["asciidocattr"]
-      if asciidocattr != nil
-        asciidocattr.each do |attr_name, attr_value|
-          attr_header = "#{attr_header}:#{attr_name}: #{attr_value.to_s}\n"
-        end
+    version = cfg["version"]
+    valias = cfg["alias"]
+    minor_str = minor_to_s(minor)
+    doc_dir = "#{minor_str}.x"
+
+    puts "Processing infinispan #{doc_dir}"
+
+    html_artifact = "org.infinispan:infinispan-docs:#{version}:zip:html"
+    javadoc_artifact = cfg.fetch("docs_javadoc", true) ? "org.infinispan:infinispan-javadoc-all:#{version}:jar:javadoc" : nil
+
+    get_maven_docs(html_artifact, javadoc_artifact, nil, "docs", doc_dir, valias)
+    vname = if valias != nil then "#{doc_dir} (#{valias})" else doc_dir end
+    coreDocIndex.push "#{vname}!#{doc_dir}"
+    %x( mvn org.apache.maven.plugins:maven-dependency-plugin:3.9.0:unpack -DoutputDirectory=schemas -DmarkersDirectory=. -Dartifact=org.infinispan:infinispan-distribution:#{version}:zip:xsd )
+  end
+
+  # Fetch Hot Rod C++ and .NET client docs from project downloads
+  hotrod = projects["projects"]
+  ["cpp-client", "dotnet-client"].each do |client|
+    project = hotrod[client]
+    next unless project
+    short_name = client.sub("-client", "")
+    project.each do |ver, vcfg|
+      next if ver == "github" || ver == "doc_branches"
+      next unless vcfg.is_a?(Hash) && vcfg["docs_version"]
+      docs_version = vcfg["docs_version"]
+      github = project["github"]
+      url = "#{github}/archive/#{vcfg["version"]}.zip"
+      %x( wget -nv #{url} -O _tmp.zip)
+      %x( unzip -o _tmp.zip "*-Source/documentation/*" -d _tmp)
+      %x( mkdir -p docs/hotrod-clients/#{short_name}/docs )
+      %x( rm _tmp/*/documentation/.gitignore )
+      %x( mv _tmp/*/documentation "docs/hotrod-clients/#{short_name}/docs/#{docs_version}" )
+      Dir.glob("docs/hotrod-clients/#{short_name}/docs/**/titles/*.{adoc,asciidoc}").each do |f|
+        %x( asciidoctor -q #{f} )
       end
+      %x( rm -rf _tmp _tmp.zip )
+    end
+  end
 
-      if type == "infinispan"
-        core = vcfg["core"]
-        valias = core["alias"]
-        get_maven_docs(core["html"], core["javadoc"], core["pdf"], "docs", "#{ver}", valias)
-        vname = if valias != nil then "#{ver} (#{valias})" else "#{ver}" end
-        coreDocIndex.push "#{vname}!#{ver}"
-        %x( mvn org.apache.maven.plugins:maven-dependency-plugin:3.9.0:unpack -DoutputDirectory=schemas -DmarkersDirectory=. -Dartifact=org.infinispan:infinispan-distribution:#{ver}:zip:xsd )
+  # Fetch simple tutorials
+  tutorials = projects["projects"]["simple-tutorials"]
+  if tutorials && tutorials["doc_branches"]
+    tutorials["doc_branches"].each do |branch|
+      fetch_github_docs(tutorials["github"], branch, "tutorials/simple", verbose)
+    end
+  end
+
+  # Fetch Hot Rod JS client docs
+  js_client = projects["projects"]["js-client"]
+  if js_client && js_client["doc_branches"]
+    js_client["doc_branches"].each do |branch|
+      fetch_github_docs(js_client["github"], branch, "docs/hotrod-clients/js/latest", verbose)
+    end
+  end
+
+  # Fetch Hot Rod C++ client docs
+  cpp_client = projects["projects"]["cpp-client"]
+  if cpp_client && cpp_client["doc_branches"]
+    cpp_client["doc_branches"].each do |branch|
+      fetch_github_docs(cpp_client["github"], branch, "docs/hotrod-clients/cpp/latest", verbose)
+    end
+  end
+
+  # Fetch Hot Rod .NET client docs
+  dotnet_client = projects["projects"]["dotnet-client"]
+  if dotnet_client && dotnet_client["doc_branches"]
+    dotnet_client["doc_branches"].each do |branch|
+      fetch_github_docs(dotnet_client["github"], branch, "docs/hotrod-clients/dotnet/latest", verbose)
+    end
+  end
+
+  # Fetch operator docs
+  operator = projects["projects"]["operator"]
+  if operator && operator["doc_branches"]
+    operator["doc_branches"].each do |branch|
+      zip_url = "#{operator["github"]}/archive/#{branch}.zip"
+      %x( wget -nv #{zip_url} -O _optmp.zip)
+      %x( unzip -o _optmp.zip "*documentation/*" -d _optmp)
+      Dir.glob("_optmp/**/*.asciidoc").each do |f|
+        %x( asciidoctor #{f} )
       end
-    end
-  end
-
-  # cpp and dotnet clients need the same special treatment
-  ["cpp", "dotnet"].each do |client|
-    cfg["hotrod"][client]["downloads"].each do |label, artifact|
-      if artifact.include?("docs_version")
-        docs_version = artifact["docs_version"]
-        url = artifact["download"]
-        fullname = File.basename(url)
-        name = File.basename(url, ".zip")
-        %x( wget -nv #{url} -O _tmp.zip)
-        %x( unzip -o _tmp.zip "*-Source/documentation/*" -d _tmp)
-        %x( mkdir -p docs/hotrod-clients/#{client}/docs )
-        %x( rm _tmp/*/documentation/.gitignore )
-        %x( mv _tmp/*/documentation "docs/hotrod-clients/#{client}/docs/#{docs_version}" )
-        # Only process the main entry point files in titles directory
-        Dir.glob("docs/hotrod-clients/#{client}/docs/**/titles/*.{adoc,asciidoc}").each do |f|
-          %x( asciidoctor -q #{f} )
-        end
-        %x( rm -rf _tmp _tmp.zip )
+      Dir.glob("_optmp/**/*.html").each do |f|
+        %x( cp -r #{f} _optmp )
       end
+      %x( mkdir -p docs/infinispan-operator/#{branch}/ )
+      %x( mkdir -p docs/infinispan-operator/topics/images/ )
+      %x( mv _optmp/*.html _optmp/**/documentation/asciidoc/css _optmp/**/documentation/asciidoc/js "docs/infinispan-operator/#{branch}/" )
+      # Use images only on main. Causes harmless "cannot stat" messages.
+      if branch == "main"
+        %x( cp -r _optmp/infinispan-operator-main/documentation/asciidoc/topics/images/* "docs/infinispan-operator/topics/images/" )
+      end
+      %x( rm -rf _optmp* )
+      operatorDocIndex.push branch
     end
   end
 
-  # Simple tutorials latest
-  cfg["simple_tutorials"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _jstmp.zip)
-    %x( unzip -o _jstmp.zip "*documentation/*" -d _jstmp)
-    Dir.glob("_jstmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
+  # Fetch Helm chart docs
+  helm = projects["projects"]["helm-charts"]
+  if helm && helm["doc_branches"]
+    helm["doc_branches"].each do |branch|
+      zip_url = "#{helm["github"]}/archive/#{branch}.zip"
+      %x( wget -nv #{zip_url} -O _charttmp.zip)
+      %x( unzip -o _charttmp.zip "*documentation/*" -d _charttmp)
+      Dir.glob("_charttmp/**/*.asciidoc").each do |f|
+        %x( asciidoctor #{f} )
+      end
+      Dir.glob("_charttmp/**/*.html").each do |f|
+        %x( cp -r #{f} _charttmp )
+      end
+      %x( mkdir -p docs/helm-chart/#{branch}/ )
+      %x( mv _charttmp/*.html _charttmp/**/documentation/asciidoc/css _charttmp/**/documentation/asciidoc/js "docs/helm-chart/#{branch}/" )
+      %x( rm -rf _charttmp* )
+      helmChartDocIndex.push branch
     end
-    Dir.glob("_jstmp/**/*.html").each do |f|
-      %x( cp -r #{f} _jstmp )
-    end
-    %x( mkdir -p tutorials/simple/ )
-    %x( mv _jstmp/*.html "tutorials/simple/" )
-    %x( rm -rf _jstmp* )
   end
 
-  # Hot Rod JS client latest
-  cfg["hr_js_client"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _jstmp.zip)
-    %x( unzip -o _jstmp.zip "*documentation/*" -d _jstmp)
-    Dir.glob("_jstmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
+  # Fetch Spring Boot starter docs
+  spring_boot = projects["projects"]["spring-boot"]
+  if spring_boot && spring_boot["doc_branches"]
+    spring_boot["doc_branches"].each do |branch|
+      fetch_github_docs(spring_boot["github"], branch, "docs/infinispan-spring-boot/#{branch}", verbose)
     end
-    Dir.glob("_jstmp/**/*.html").each do |f|
-      %x( cp -r #{f} _jstmp )
-    end
-    %x( mkdir -p docs/hotrod-clients/js/latest/ )
-    %x( mv _jstmp/*.html "docs/hotrod-clients/js/latest/" )
-    %x( rm -rf _jstmp* )
-  end
-
-  # Hot Rod C++ client latest
-  cfg["hr_cpp_client"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _cpptmp.zip)
-    %x( unzip -o _cpptmp.zip "*documentation/*" -d _cpptmp)
-    Dir.glob("_cpptmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
-    end
-    Dir.glob("_cpptmp/**/*.html").each do |f|
-      %x( cp -r #{f} _cpptmp )
-    end
-    %x( mkdir -p docs/hotrod-clients/cpp/latest/ )
-    %x( mv _cpptmp/*.html "docs/hotrod-clients/cpp/latest/" )
-    %x( rm -rf _cpptmp* )
-  end
-
-  # Hot Rod .NET/C# client latest
-  cfg["hr_dotnet_client"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _dotnettmp.zip)
-    %x( unzip -o _dotnettmp.zip "*documentation/*" -d _dotnettmp)
-    Dir.glob("_dotnettmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
-    end
-    Dir.glob("_dotnettmp/**/*.html").each do |f|
-      %x( cp -r #{f} _dotnettmp )
-    end
-    %x( mkdir -p docs/hotrod-clients/dotnet/latest/ )
-    %x( mv _dotnettmp/*.html "docs/hotrod-clients/dotnet/latest/" )
-    %x( rm -rf _dotnettmp* )
-  end
-
-  # and then it's operator's turn
-  cfg["ispn_operator"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _optmp.zip)
-    %x( unzip -o _optmp.zip "*documentation/*" -d _optmp)
-    Dir.glob("_optmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
-    end
-    Dir.glob("_optmp/**/*.html").each do |f|
-      %x( cp -r #{f} _optmp )
-    end
-    %x( mkdir -p docs/infinispan-operator/#{version}/ )
-    %x( mkdir -p docs/infinispan-operator/topics/images/ )
-    %x( mv _optmp/*.html _optmp/**/documentation/asciidoc/css _optmp/**/documentation/asciidoc/js "docs/infinispan-operator/#{version}/" )
-    #Use images only on main. Causes harmless "cannot stat" messages.
-    %x( cp -r _optmp/infinispan-operator-main/documentation/asciidoc/topics/images/* "docs/infinispan-operator/topics/images/" )
-    %x( rm -rf _optmp* )
-    valias = sub["alias"]
-    vname = if valias != nil then "#{version} (#{valias})" else "#{version}"
-    end
-    operatorDocIndex.push "#{vname}!#{version}"
-  end
-
-  # Helm chart docs
-  cfg["helm_chart"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _charttmp.zip)
-    %x( unzip -o _charttmp.zip "*documentation/*" -d _charttmp)
-    Dir.glob("_charttmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
-    end
-    Dir.glob("_charttmp/**/*.html").each do |f|
-      %x( cp -r #{f} _charttmp )
-    end
-    %x( mkdir -p docs/helm-chart/#{version}/ )
-    %x( mv _charttmp/*.html _charttmp/**/documentation/asciidoc/css _charttmp/**/documentation/asciidoc/js "docs/helm-chart/#{version}/" )
-    %x( rm -rf _charttmp* )
-    helmChartDocIndex.push "#{version}"
-  end
-
-  # now for the spring boot starter docs
-  cfg["sb_starter"].each do |version, sub|
-    zipUrl = sub["zip-url"]
-    %x( wget -nv #{zipUrl} -O _sbtmp.zip)
-    %x( unzip -o _sbtmp.zip "*documentation/*" -d _sbtmp)
-    Dir.glob("_sbtmp/**/*.asciidoc").each do |f|
-      %x( asciidoctor #{f} )
-    end
-    Dir.glob("_sbtmp/**/*.html").each do |f|
-      %x( cp -r #{f} _sbtmp )
-    end
-    %x( mkdir -p docs/infinispan-spring-boot/#{version}/ )
-    %x( mv _sbtmp/*.html "docs/infinispan-spring-boot/#{version}/" )
-    %x( rm -rf _sbtmp* )
   end
 
   gen_versions_xml_file("docs/infinispan-operator/versions.xml", operatorDocIndex)
